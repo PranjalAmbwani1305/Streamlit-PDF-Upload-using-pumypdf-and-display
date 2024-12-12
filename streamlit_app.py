2a557fa8d325fde788b4b90615d6e927452eb717
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-12 17:57:39 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,20 @@ def main():
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
     if uploaded_file is not None:
-        # Access the uploaded file name (path)
+        
         file_path = uploaded_file.name
         with open(file_path, "rb") as f:
             doc = fitz.open(f)
 
-            # Process the PDF document
+           
             for page_num in range(doc.page_count):
                 page = doc[page_num]
-                # Example: Extract text from the page
+              
                 text = page.get_text()
                 st.write(f"Page {page_num + 1} Text:")
                 st.write(text)
 
-                # Example: Convert page to image
+            
                 pix = page.get_pixmap()
                 st.image(pix.tobytes())
 
