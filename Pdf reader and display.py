b76e98e69b77936869d931e7e72e3ed28f6c7e14
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-12 17:19:22 +0530

    Update Pdf reader and display.py

--- Pdf reader and display.py
+++ Pdf reader and display.py
@@ -1,5 +1,7 @@
-import streamlit
+import streamlit as st
 import fitz
+from io import BytesIO
+from PIL import Image
 
 def display_pdf(pdf_file):
     doc = fitz.open(pdf_file)
@@ -8,14 +10,17 @@ def display_pdf(pdf_file):
     for page_num in range(num_pages):
         page = doc.load_page(page_num)  
         pix = page.get_pixmap() 
-        img = pix.tobytes("png") 
+        
+        
+        img = Image.open(BytesIO(pix.tobytes("png")))
+        
+       
         st.image(img, caption=f"Page {page_num + 1}", use_column_width=True)
 
 st.title("PDF READER")
 
-
 pdf_file = st.file_uploader("Upload a PDF", type="pdf")
-
 
 if pdf_file is not None:
     display_pdf(pdf_file)
+
