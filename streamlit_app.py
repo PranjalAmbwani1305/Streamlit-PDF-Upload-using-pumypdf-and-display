673ef7ebe89891cc0872e0efa2db43df9f10b34b
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-12 17:59:05 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,11 @@ def main():
         file_path = uploaded_file.name
         with open(file_path, "rb") as f:
             doc = fitz.open(f)
-
-           
             for page_num in range(doc.page_count):
                 page = doc[page_num]
-              
                 text = page.get_text()
                 st.write(f"Page {page_num + 1} Text:")
                 st.write(text)
-
-            
                 pix = page.get_pixmap()
                 st.image(pix.tobytes())
 
