9d0bd1161a0d3250d730e40c6e85323c84a0e2f3
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-13 10:09:18 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,29 @@ import streamlit as st
 import fitz
 
 def main():
-    st.title("PDF Viewer App ")
+    st.title("PDF Viewer App")
 
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
     if uploaded_file is not None:
-       
         pdf_bytes = uploaded_file.read()
         pdf_document = fitz.open(stream=pdf_bytes)
 
-   
-        total_pages = len(pdf_document)
-        st.write(f"Total Pages: {total_pages}")
+        st.write(f"Total Pages: {len(pdf_document)}")
 
-     
-        for page_number in range(total_pages):
-            
-            page = pdf_document[page_number]
-
-           page_number = st.number_input(
+        page_number = st.number_input(
             "Select page number",
             min_value=1,
             max_value=len(pdf_document),
             value=1
-            )
-            pix = page.get_pixmap()
+        )
 
-          
-            img_bytes = pix.tobytes()
+        page = pdf_document[page_number - 1]
+        pix = page.get_pixmap()
+        img_bytes = pix.tobytes()
 
-            
-            st.image(img_bytes, use_column_width=True)
+        st.image(img_bytes, caption=f"Page {page_number}", use_column_width=True)
 
-      
         pdf_document.close()
 
 if __name__ == "__main__":
